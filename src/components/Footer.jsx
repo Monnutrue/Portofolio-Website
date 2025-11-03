8731d2aa96749d32be93720d2e775c97ba54a7c0
Monnutrue <dafaalfarizi1411@gmail.com>
2025-11-03 09:15:32 +0700

    Making responsive ui on android and tab

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,28 +1,32 @@
 const Footer = () => {
   return (
     <div className="footer mt-10">
-        <div className="xl:w-1/1 lg:w-3/4 w-full pt-10 pb-10 mx-auto bg-zinc-800 rounded-t-3xl grid grid-cols-2">
-            <div className="md:ps-20 xl:ps-40 ps-5">
+        <div className="xl:w-1/1 lg:w-3/4 w-full pt-10 pb-10 md:ps-0 ps-10 mx-auto bg-zinc-800 rounded-t-3xl grid md:grid-cols-2 sm:grid-cols-2 grid-cols-1">
+            <div className="md:ps-20 xl:ps-40 ps-0">
               <h4 className="md:text-4xl text-[15pt] font-bold">Contact</h4>
             </div>
             <div className="md:ps-10">
               <h4 className="md:text-4xl text-[15pt] font-bold">Social Media</h4>
-              <ul className="md:pt-5 md:mx-6 pt-1">
+              <ul className="sm:pt-3 sm:mx-6 sm:me-0 me-10 pt-1 sm:grid sm:grid-cols-1 flex items-center justify-between">
                 <li className="mb-1">
-                  <a href="" className="md:flex grid items-center md:gap-2" >
-                    <i className="ri-facebook-box-fill ri-2x"></i>Muhammad Dafa Alfarizi</a>
+                  <a href="" className="flex items-center sm:gap-2" >
+                    <i className="ri-facebook-box-fill ri-2x"></i>
+                    <p className="sm:flex hidden">Muhammad Dafa Alfarizi</p></a>
                 </li>
                 <li className="mb-1">
-                  <a href="" className="md:flex grid items-center md:gap-2" >
-                    <i class="ri-instagram-line ri-2x"></i> @dafaalfarizi1411</a>
+                  <a href="" className="flex items-center sm:gap-2" >
+                    <i class="ri-instagram-line ri-2x"></i>
+                    <p className="sm:flex hidden">@dafaalfarizi1411</p></a>
                 </li>
                 <li className="mb-1">
-                  <a href="" className="md:flex grid items-center md:gap-2" >
-                    <i class="ri-linkedin-box-fill ri-2x"></i> Muhammad Dafa Alfarizi</a>
+                  <a href="" className="flex items-center sm:gap-2" >
+                    <i class="ri-linkedin-box-fill ri-2x"></i>
+                    <p className="sm:flex hidden">Muhammad Dafa Alfarizi</p></a>
                 </li>
                 <li>
-                  <a href="" className="md:flex grid items-center md:gap-2" >
-                    <i class="ri-github-fill ri-2x"></i> Monnutrue</a>
+                  <a href="" className="flex items-center sm:gap-2" >
+                    <i class="ri-github-fill ri-2x"></i>
+                    <p className="sm:flex hidden">Monnutrue</p></a>
                 </li>
               </ul>
             </div>
